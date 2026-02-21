3a3913b8a55aee2caa19fa35eb8846cfd3ac5b75
Sudharshan1305 <sudharshankrishnaalk@gmail.com>
2026-02-21 09:47:39 +0530

    UI: wire sidebar and topbar into layout

--- frontend/client/src/components/layout/Layout.jsx
+++ frontend/client/src/components/layout/Layout.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Sidebar from "./Sidebar";
+import Topbar from "./Topbar";
 
 function Layout({ children }) {
     return (
         <div style={{ display: "flex", minHeight: "100vh" }}>
-            {children}
+            {/* Left Sidebar */}
+            <Sidebar />
+
+            {/* Right Main Area */}
+            <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
+                <Topbar />
+                <div style={{ padding: "24px", flex: 1 }}>{children}</div>
+            </div>
         </div>
     );
 }
